6a0d93cb5e3f86bbee5f32a00eb1bae87b5439d5
Nuttapat Pothavichai <nuttapatp@outlook.co.th>
2024-02-05 22:48:36 +0700

    1st revise main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,6 +23,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Main.class, args); // Start the Spring Boot application
+        initializeFirebase();
 
     }
 
